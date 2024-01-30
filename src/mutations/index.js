66c68184eee3213a38ce3fd57aa86cc7990ba6c8
shoaibfarooq3 <shoaibfarooq3@gmail.com>
2024-01-30 13:13:04 +0500

    add: FAQ complete CRUD operation

--- src/mutations/index.js
+++ src/mutations/index.js
@@ -1,6 +1,10 @@
 import contactUs from './contactUs.js';
-import createFaq from './createFaq.js'
+import createFaq from './createFaq.js';
+import deleteFaq from './deleteFaq.js';
+import updateFaq from './updateFaq.js'
 export default{
     contactUs,
-    createFaq
+    createFaq,
+    deleteFaq,
+    updateFaq
 }
--- src/resolvers/Mutation/index.js
+++ src/resolvers/Mutation/index.js
@@ -1,6 +1,11 @@
 import createFaq from "./createFaq.js";
 import contactUs from "./contactUs.js";
+import deleteFaq from "./deleteFaq.js";
+import updateFaq from "./updateFaq.js";
 export default {
     createFaq,
-    contactUs
+    contactUs,
+    deleteFaq,
+    updateFaq,
+
 }
