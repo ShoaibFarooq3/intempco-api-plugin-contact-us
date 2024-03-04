b37a619a8d3fc08be43d6a5418d5e23c52c55635
shoaibfarooq3 <shoaibfarooq3@gmail.com>
2024-03-04 14:54:07 +0500

    add: CRUD operation for company Directory

--- src/resolvers/Mutation/index.js
+++ src/resolvers/Mutation/index.js
@@ -4,11 +4,19 @@ import deleteFaq from "./deleteFaq.js";
 import updateFaq from "./updateFaq.js";
 import createContactDetails from "./createContactDetails.js";
 import updateContactDetails from "./updateContactDetails.js";
+import deleteContactDetails from "./deleteContactDetails.js";
+import createCompanyDirectory from "./createCompanyDirectory.js";
+import updateCompanyDirectory from "./updateCompanyDirectory.js";
+import deleteCompanyDirectory from "./deleteCompanyDirectory.js";
 export default {
     createFaq,
     contactUs,
     deleteFaq,
     updateFaq,
     createContactDetails,
-    updateContactDetails
+    updateContactDetails,
+    deleteContactDetails,
+    createCompanyDirectory,
+    updateCompanyDirectory,
+    deleteCompanyDirectory
 }
--- src/schemas/index.js
+++ src/schemas/index.js
@@ -3,4 +3,5 @@ import importAsString from "@reactioncommerce/api-utils/importAsString.js";
 const schema = importAsString("./schema.graphql");
 const faq = importAsString("./faq.graphql");
 const contactUsDetail = importAsString("./contactUsDetail.graphql");
-export default [schema, faq,contactUsDetail];
+const companyDirectory = importAsString("./companyDirectory.graphql");
+export default [schema, faq, contactUsDetail, companyDirectory];
